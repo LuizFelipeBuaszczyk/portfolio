b9f319dbb74f246d9a769b8f91947d05c665610b
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2026-02-17 12:42:49 -0300

    style: Melhoria no estilo dos projetos e padronização da lista de projetos e adicionando mais um projeto

--- src/pages/Projects/sections/ProjectsSection/ProjectsSection.jsx
+++ src/pages/Projects/sections/ProjectsSection/ProjectsSection.jsx
@@ -5,21 +5,38 @@ import ProjectCard from "../../../../components/ProjectCard/ProjectCard";
 import { LanguageContext } from "../../../../contexts/LanguageContext";
 
 import imgUlaFPGA from "../../../../assets/images/projects/ula-fpga-vhdl.png";
+import imgRotaexpress from "../../../../assets/images/projects/rotaexpress-logo.png";
 
-const ula_fpga = {
-    "pt-br": {
-        imageSrc:imgUlaFPGA,
-        imageAlt:"FPGA ULA",
-        title: "ULA com FPGA",
-        description: "Este projeto teve o objetivo de desenvolver um ULA de 4 bits com VHDL e FPGA. A ULA possui operações de soma, subtração e comparação."
+const projects = [
+    {
+        "pt-br": {
+            imageSrc:imgUlaFPGA,
+            imageAlt:"FPGA ULA",
+            title: "ULA com FPGA",
+            description: "Este projeto teve o objetivo de desenvolver um ULA de 4 bits com VHDL e FPGA. A ULA possui operações de soma, subtração e comparação."
+        },
+        "en-us": {
+            imageSrc:imgUlaFPGA,
+            imageAlt:"FPGA ALU",
+            title: "ALU with FPGA",
+            description: "This project had the objective of developing a 4-bit ALU with VHDL and FPGA. The ALU has sum, subtract, and comparator operations." 
+        }
     },
-    "en-us": {
-        imageSrc:imgUlaFPGA,
-        imageAlt:"FPGA ALU",
-        title: "ALU with FPGA",
-        description: "This project had the objective of developing a 4-bit ALU with VHDL and FPGA. The ALU has sum, subtract, and comparator operations." 
+    {
+        "pt-br": {
+            imageSrc:imgRotaexpress,
+            imageAlt:"Rotaexpress Logo",
+            title: "Rotaexpress",
+            description: "Este projeto teve o objetivo de desenvolver um sistema para auxilio nas entregas, disponibilizando uma seção de criação de empresas, membros e entregas. Possui integração com o google Maps para geração e visualização das rotas."
+        },
+        "en-us": {
+            imageSrc:imgRotaexpress,
+            imageAlt:"Rotaexpress Logo",
+            title: "Rotaexpress",
+            description: "This project had the objective of developing a system to help with deliveries, providing a section for creating companies, members and deliveries. It has integration with google Maps to generate route and visualization." 
+        }
     }
-}
+]
 
 
 function ProjectsSection(){
@@ -27,7 +44,9 @@ function ProjectsSection(){
 
     return (
         <section id="project-section">
-            <ProjectCard projectData={ula_fpga[language]}/>
+            {projects.map(project => (
+                 <ProjectCard projectData={project[language]}/>    
+            ))}; 
         </section>
     );
 }
